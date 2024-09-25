d049ad99b9cc631aec770f1cb449cde0e6be215e
maneesh16m <maneesh16m@gmail.com>
2024-09-25 11:26:42 +1000

    added

--- src/data/constants.js
+++ src/data/constants.js
@@ -2,12 +2,12 @@ export const ProductsData  = [
   {
     title: "WALLET SaaS",
     description: "A custom-built gift card management system with dedicated admin & customer portal. Easily create, distribute, and track both physical and digital gift cards to engage customers and drive sales",
-    href: "http://voucha.nexzap.com"
+    href: "http://wallet.nexzap.com"
   },
   {
     title: "PRIVATEBOT SaaS",
     description:"A cutting-edge, LLM-based Retrieval-Augmented Generation (RAG) model designed to help companies organize and retrieve information efficiently. With RAGBOT, businesses can leverage AI for intelligent document search and processing.",
-    href: "http://lucas.chatbot.nexzap.com"
+    href: "http://chatbot.nexzap.com"
   },
 ];
   
@@ -17,7 +17,7 @@ export const ProductsData  = [
     {
       id:1,
       title: "SaaS Products",
-      description: "Sell, redeem, and recharge gift cards both online and through POS terminals.Sell, redeem, and recharge gift cards both online and through POS terminals.",
+      description: "We build customised SaaS products for your business.",
       tags: ["Arduino UNO", "Ultrasonic Sensor","Buzzer", "Code in C++"],
     },
     {
